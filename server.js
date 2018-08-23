3fa29df07f6a902781f13b381492b11a7f27758f
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-08-23 20:55:56 +0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.set('view engine','ejs')
+app.get('/',(req,res) => {
+    res.render('index')
+})
+
+app.listen(port)
+
+console.log(`Server Complete @ Port ${port}`);
